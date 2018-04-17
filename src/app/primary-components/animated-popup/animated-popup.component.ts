cce4356c8355a1c5fc660ffed4b2a016e59e8d4c
MNakel <MNakel@SISFirst.com>
2018-04-17 14:55:15 -0400

    Modified animated-modal functionality and styling.

--- src/app/primary-components/animated-popup/animated-popup.component.ts
+++ src/app/primary-components/animated-popup/animated-popup.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnimatedPopupComponent implements OnInit {
 
+  isModalDisplayed:boolean=false;
+  modalDisplay:string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  displayModal() {
+    if(this.isModalDisplayed == false) {
+      this.isModalDisplayed = true;
+      this.modalDisplay = "inline-block";
+    }
+    else {
+      this.isModalDisplayed = false;
+      this.modalDisplay = "none";
+    }
   }
 
 }
