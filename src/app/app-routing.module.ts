0e1815f769b2de716d1312d3176ae8570678dfcf
Max <maxnakel@gmail.com>
2018-06-19 17:50:45 -0400

    Relocated routing module and removed comments.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainContentComponent } from './primary-components/landing-page/main-content/main-content.component';
-import { SlidingDoorsComponent } from './primary-components/sliding-doors/sliding-doors.component';
-import { AnimatedPopupComponent } from './primary-components/animated-popup/animated-popup.component';
+import { MainContentComponent } from './bulma-fire/primary-components/landing-page/main-content/main-content.component';
+import { SlidingDoorsComponent } from './bulma-fire/primary-components/sliding-doors/sliding-doors.component';
+import { AnimatedPopupComponent } from './bulma-fire/primary-components/animated-popup/animated-popup.component';
 import { CommonModule } from '@angular/common';
 
 const appRoutes: Routes = [
@@ -10,16 +10,15 @@ const appRoutes: Routes = [
     { path: "dashboard", component: MainContentComponent },
     { path: "slidingDoors", component: SlidingDoorsComponent },
     { path: 'animatedPopup', component: AnimatedPopupComponent }
-  ]
+];
 
   @NgModule({
-    declarations: [
-    ],
+    declarations: [],
     imports: [
       CommonModule,
       RouterModule.forRoot(appRoutes)
     ],
-    exports: [RouterModule],
+    exports: [ RouterModule ],
     providers: [],
     bootstrap: []
   })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { FooterSectionComponent } from './bulma-fire/primary-components/landing-page/footer-section/footer-section.component';
 import { MainContentComponent } from './bulma-fire/primary-components/landing-page/main-content/main-content.component';
@@ -10,14 +9,7 @@ import { SlidingDoorsComponent } from './bulma-fire/primary-components/sliding-d
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StepLadderComponent } from './bulma-fire/primary-components/step-ladder/step-ladder.component';
 import { AnimatedPopupComponent } from './bulma-fire/primary-components/animated-popup/animated-popup.component';
-import { AppRoutingModule } from './bulma-fire/app-routing.module';
-
-// const appRoutes: Routes = [
-//   { path: "", redirectTo: "/dashboard", pathMatch: "full" },
-//   { path: "dashboard", component: MainContentComponent },
-//   { path: "slidingDoors", component: SlidingDoorsComponent },
-//   { path: 'animatedPopup', component: AnimatedPopupComponent }
-// ]
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -32,7 +24,6 @@ import { AppRoutingModule } from './bulma-fire/app-routing.module';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule
-    // RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
